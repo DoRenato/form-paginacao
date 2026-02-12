d75260cc140fc50894604e96458301917698ea6f
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-02-12 12:48:57 -0300

    style: adjust main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import FormDemo from "@/features/form-demo/FormDemo";
 
 export default function Home() {
   return (
-    <main className="container mx-auto">
-      <div className="pt-2 grid grid-cols-2">
-        <div>
-          <FormDemo />
+    <main className="bg-gray-300 lg:min-h-dvh">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-12 pt-2">
+          <div className="col-span-8">
+            <FormDemo />
+          </div>
+          <div className="col-span-4">lista</div>
         </div>
-        <div>lista</div>
       </div>
     </main>
   );
