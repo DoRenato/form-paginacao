6d0653728a2c9f7150061bbf99f2ad9d19b34d87
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-02-12 22:39:44 -0300

    feat(page): add accounts display widget

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import FormDemo from "@/features/form-demo/FormDemo";
+import ShowAccounts from "@/widgets/ShowAccounts";
 
 export default function Home() {
   return (
-    <main className="bg-gray-300 lg:min-h-dvh">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-12 pt-2">
-          <div className="col-span-8">
-            <FormDemo />
-          </div>
-          <div className="col-span-4">lista</div>
+    <main className="relative bg-gray-100 lg:min-h-dvh">
+      <div className="grid grid-cols-12 px-2 pt-2">
+        <div className="col-span-7 px-5">
+          <FormDemo />
+        </div>
+      </div>
+      <div className="absolute top-0 right-0 h-full w-200 bg-white px-5">
+        <div className="">
+          <ShowAccounts />
         </div>
       </div>
     </main>
