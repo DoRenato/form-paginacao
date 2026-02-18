f24f966ada8218f7af6451ec75964912ba55bbbe
DoRenato <64191958+DoRenato@users.noreply.github.com>
2026-02-17 22:50:45 -0300

    feat(form-demo): move to widgets folder

--- widgets/FormDemo.tsx
+++ widgets/FormDemo.tsx
@@ -3,29 +3,29 @@
 import { toast } from "sonner";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { useFormDemo } from "./useFormDemo";
-import { NameField } from "./fields/NameField";
-import { LastNameField } from "./fields/LastNameField";
-import { FormValues } from "./schema";
+import { useFormDemo } from "@/features/form-demo/useFormDemo";
+import { NameField } from "@/features/form-demo/fields/NameField";
+import { LastNameField } from "@/features/form-demo/fields/LastNameField";
+import { FormValues } from "@/features/form-demo/schema";
 import { FormProvider } from "react-hook-form";
-import PhoneNumbers from "./fields/PhoneNumbersField";
-import { CpfField } from "./fields/CpfField";
-import { BirthDateField } from "./fields/BirthDateField";
-import GenderField from "./fields/GenderField";
+import PhoneNumbers from "@/features/form-demo/fields/PhoneNumbersField";
+import { CpfField } from "@/features/form-demo/fields/CpfField";
+import { BirthDateField } from "@/features/form-demo/fields/BirthDateField";
+import GenderField from "@/features/form-demo/fields/GenderField";
 import FormSection from "@/components/form/FormSection";
-import { StreetField } from "./fields/address/StreetField";
-import { AddressNumberField } from "./fields/address/AddressNumberField";
-import { ComplementField } from "./fields/address/ComplementField";
-import { NeighborhoodField } from "./fields/address/NeighborhoodField";
-import { ZipCodeField } from "./fields/address/ZipCodeField";
-import { CityField } from "./fields/address/CityField";
-import StateField from "./fields/address/StateField";
-import { EmailField } from "./fields/EmailField";
-import PhoneNotificationField from "./fields/notifications/PhoneNotificationField";
-import { CheckboxesTitleField } from "./fields/CheckboxesTitleField";
-import EmailNotificationField from "./fields/notifications/EmailNotificationField";
+import { StreetField } from "@/features/form-demo/fields/address/StreetField";
+import { AddressNumberField } from "@/features/form-demo/fields/address/AddressNumberField";
+import { ComplementField } from "@/features/form-demo/fields/address/ComplementField";
+import { NeighborhoodField } from "@/features/form-demo/fields/address/NeighborhoodField";
+import { ZipCodeField } from "@/features/form-demo/fields/address/ZipCodeField";
+import { CityField } from "@/features/form-demo/fields/address/CityField";
+import StateField from "@/features/form-demo/fields/address/StateField";
+import { EmailField } from "@/features/form-demo/fields/EmailField";
+import PhoneNotificationField from "@/features/form-demo/fields/notifications/PhoneNotificationField";
+import { CheckboxesTitleField } from "@/features/form-demo/fields/CheckboxesTitleField";
+import EmailNotificationField from "@/features/form-demo/fields/notifications/EmailNotificationField";
 import { Separator } from "@/components/ui/separator";
-import MultiStepSwitchField from "./fields/MultiStepSwitchField";
+import MultiStepSwitchField from "@/features/form-demo/fields/MultiStepSwitchField";
 import React from "react";
 import { UserPayload } from "@/data/listOfAccounts";
 
@@ -77,26 +77,26 @@ export default function FormDemo({ setAccounts }: FormDemoProps) {
             titulo="Dados Pessoais"
             descricao="Os campos como CPF e Telefone possuem máscaras para melhor experiência."
           />
-          <div className="grid grid-cols-12 gap-3">
-            <div className="col-span-3">
+          <div className="grid grid-cols-2 gap-3 lg:grid-cols-12">
+            <div className="order-1 lg:order-0 lg:col-span-3">
               <NameField control={form.control} />
             </div>
-            <div className="col-span-3">
+            <div className="order-2 lg:order-0 lg:col-span-3">
               <LastNameField control={form.control} />
             </div>
-            <div className="col-span-3">
+            <div className="order-3 lg:order-0 lg:col-span-3">
               <CpfField control={form.control} />
             </div>
-            <div className="col-span-3 row-span-2">
+            <div className="order-7 col-span-2 lg:order-0 lg:col-span-3 lg:row-span-2">
               <GenderField />
             </div>
-            <div className="col-span-4">
+            <div className="order-4 lg:order-0 lg:col-span-4">
               <EmailField control={form.control} />
             </div>
-            <div className="col-span-2">
+            <div className="order-6 lg:order-0 lg:col-span-2">
               <BirthDateField />
             </div>
-            <div className="col-span-3">
+            <div className="order-5 lg:order-0 lg:col-span-3">
               <PhoneNumbers control={form.control} />
             </div>
           </div>
@@ -107,29 +107,29 @@ export default function FormDemo({ setAccounts }: FormDemoProps) {
             titulo="Dados de Endereço"
             descricao="Ao digitar o CEP, ele irá resgatar todos os dados de endereço do mesmo."
           />
-          <div className="w-30 pb-5">
+          <div className="w-35 lg:w-30 pb-5">
             <ZipCodeField control={form.control} />
           </div>
-          <div className="grid grid-cols-12 gap-3">
-            <div className="col-span-3">
+          <div className="grid grid-cols-2 lg:grid-cols-12 gap-3">
+            <div className="lg:col-span-3">
               <StreetField control={form.control} />
             </div>
-            <div className="col-span-1">
+            <div className="lg:col-span-1">
               <AddressNumberField control={form.control} />
             </div>
-            <div className="col-span-4">
+            <div className="col-span-2 lg:col-span-4">
               <ComplementField control={form.control} />
             </div>
-            <div className="col-span-3">
+            <div className="lg:col-span-3">
               <NeighborhoodField control={form.control} />
             </div>
-            <div className="col-span-3">
+            <div className="lg:col-span-3">
               <CityField control={form.control} />
             </div>
-            <div className="col-span-3">
+            <div className="lg:col-span-3">
               <StateField control={form.control} />
             </div>
-            <div className="col-span-3">{/* espaço */}</div>
+            <div className="hidden lg:col-span-3">{/* espaço */}</div>
           </div>
           <div className="pt-2 pb-4">
             <Separator />
